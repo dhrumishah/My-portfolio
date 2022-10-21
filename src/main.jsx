d80c08fe368e8a0fac5130b9e52e232affd07abd
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-10-21 19:42:10 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import Nav from "./Nav";
 import About from "./About";
 import Intro from "./Intro";
+import Project from "./Project";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Nav />
     <About />
     <Intro />
+    <Project />
   </React.StrictMode>
 );
