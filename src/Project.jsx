a11d0ec892bb4d9f5039d196c963be9daa171f10
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-10-25 18:07:40 +0530

    projects added

--- src/Project.jsx
+++ src/Project.jsx
@@ -10,6 +10,18 @@ export default function Project() {
   function notesDeployedClick() {
     window.open("https://notesapp-dhrumishah.netlify.app/");
   }
+  function memeGenClick() {
+    window.open("https://github.com/dhrumishah/MemeGenerator");
+  }
+  function memeGenDeployedClick() {
+    window.open("https://memegenerator-dhrumishah.netlify.app/");
+  }
+  function airBnbClick() {
+    window.open("https://github.com/dhrumishah/AirBnb-clone-app");
+  }
+  function airBnbDeployedClick() {
+    window.open("https://effulgent-gelato-31ff50.netlify.app/");
+  }
   return (
     <div className="project--div">
       <h1 className="project--title">Projects I have built</h1>
@@ -30,6 +42,45 @@ export default function Project() {
               className="project--links"
               src={Github}
               onClick={notesAppClick}
+            ></img>
+          </div>
+        </div>
+      </div>
+      <div className="project--container">
+        <div className="project--desc">
+          <h2 className="desc--title-2" onClick={memeGenClick}>
+            Meme Generator
+          </h2>
+          <div className="desc--para-2">
+            <p>
+              A Meme Generator App made with ReactJS. It basically generates a
+              random meme when you apply top and bottom text.
+            </p>
+            <img
+              className="project--links-2"
+              src={Github}
+              onClick={memeGenClick}
+            ></img>
+          </div>
+        </div>
+        <div className="project--img-2" onClick={memeGenDeployedClick}>
+          <div className="project--overlay"></div>
+        </div>
+      </div>
+      <div className="project--container">
+        <div className="project--img-3" onClick={airBnbDeployedClick}>
+          <div className="project--overlay"></div>
+        </div>
+        <div className="project--desc">
+          <h2 className="desc--title" onClick={airBnbClick}>
+            AirBnB Clone
+          </h2>
+          <div className="desc--para-3">
+            <p>A basic clone app of AirBnb website. It's made with React.</p>
+            <img
+              className="project--links"
+              src={Github}
+              onClick={airBnbClick}
             ></img>
           </div>
         </div>
