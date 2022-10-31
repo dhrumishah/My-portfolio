0288f4f176b838a58913fe572d9ed797a926bc91
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-10-31 21:53:40 +0530

    added global component

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
+import Nav from "./Nav";
 import Blog_1 from "./images/blog-1.jpg";
 import Blog_2 from "./images/blog-2.jpg";
 import Blog_3 from "./images/blog-3.jpg";
@@ -22,73 +23,79 @@ export default function Blog() {
     window.open("https://dhrumishah.hashnode.dev/javascript-data-types");
   }
   return (
-    <div>
-      <h1 className="blog--title">My writings</h1>
-      <div className="blog-container">
-        <div className="blog--image">
-          <img src={Blog_1} className="blog-1--image"></img>
-        </div>
-        <div className="blog--writing">
-          <h2 className="blog--title">JavaScript Functions</h2>
-          <p>
-            Functions are one of the major pillars of JavaScript. Many
-            programming languages support various paradigms, the most well-known
-            of which being Object-Oriented and Functional Programming. Although
-            JavaScript can be both. Today in this blog, we will un...
-          </p>
-          <div className="blog--readmore">
-            <p onClick={readMore}>Read more...</p>
+    <>
+      <Nav />
+      <div>
+        <h1 className="blog--title">My writings</h1>
+        <div className="blog-container">
+          <div className="blog--image">
+            <img src={Blog_1} className="blog-1--image"></img>
+          </div>
+          <div className="blog--writing">
+            <h2 className="blog--title">JavaScript Functions</h2>
+            <p>
+              Functions are one of the major pillars of JavaScript. Many
+              programming languages support various paradigms, the most
+              well-known of which being Object-Oriented and Functional
+              Programming. Although JavaScript can be both. Today in this blog,
+              we will un...
+            </p>
+            <div className="blog--readmore">
+              <p onClick={readMore}>Read more...</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="blog-container">
-        <div className="blog--image">
-          <img src={Blog_2} className="blog-1--image"></img>
-        </div>
-        <div className="blog--writing">
-          <h2 className="blog--title">Conditional Statements in JavaScript</h2>
-          <p>
-            Have you ever wondered how a computer game or program makes
-            decisions? Or does it allow the user to decide what occurs next?
-            There are various ways that we, as programmers, can control the flow
-            or execution of a program today, we'll go through the Co...
-          </p>
-          <div className="blog--readmore">
-            <p onClick={readMoreBlog2}>Read more...</p>
+        <div className="blog-container">
+          <div className="blog--image">
+            <img src={Blog_2} className="blog-1--image"></img>
+          </div>
+          <div className="blog--writing">
+            <h2 className="blog--title">
+              Conditional Statements in JavaScript
+            </h2>
+            <p>
+              Have you ever wondered how a computer game or program makes
+              decisions? Or does it allow the user to decide what occurs next?
+              There are various ways that we, as programmers, can control the
+              flow or execution of a program today, we'll go through the Co...
+            </p>
+            <div className="blog--readmore">
+              <p onClick={readMoreBlog2}>Read more...</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="blog-container">
-        <div className="blog--image">
-          <img src={Blog_3} className="blog-1--image"></img>
-        </div>
-        <div className="blog--writing">
-          <h2 className="blog--title">JavaScript Data Types</h2>
-          <p>
-            Hey readers! This is a blog on JavaScript Data Types. I have tried
-            to cover all the possible points in this blog...
-          </p>
-          <div className="blog--readmore">
-            <p onClick={readMoreBlog3}>Read more...</p>
+        <div className="blog-container">
+          <div className="blog--image">
+            <img src={Blog_3} className="blog-1--image"></img>
+          </div>
+          <div className="blog--writing">
+            <h2 className="blog--title">JavaScript Data Types</h2>
+            <p>
+              Hey readers! This is a blog on JavaScript Data Types. I have tried
+              to cover all the possible points in this blog...
+            </p>
+            <div className="blog--readmore">
+              <p onClick={readMoreBlog3}>Read more...</p>
+            </div>
           </div>
         </div>
+        <div className="about--arrows">
+          <img
+            className="left--arrows"
+            onClick={() => {
+              navigate("/Project");
+            }}
+            src={LeftArrows}
+          ></img>
+          <img
+            className="right--arrows"
+            onClick={() => {
+              navigate("/Contact");
+            }}
+            src={RightArrows}
+          ></img>
+        </div>
       </div>
-      <div className="about--arrows">
-        <img
-          className="left--arrows"
-          onClick={() => {
-            navigate("/Project");
-          }}
-          src={LeftArrows}
-        ></img>
-        <img
-          className="right--arrows"
-          onClick={() => {
-            navigate("/Contact");
-          }}
-          src={RightArrows}
-        ></img>
-      </div>
-    </div>
+    </>
   );
 }
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
+import Nav from "./Nav";
 import Twitter from "./images/twitter.svg";
 import Hashnode from "./images/hashnode.svg";
 import Github from "./images/github.svg";
@@ -21,87 +22,90 @@ export default function Contact() {
     window.open("https://www.linkedin.com/in/dhrumi-shah-a35b751b9/");
   }
   return (
-    <div>
-      <h1 className="contact--title">Looking to Collaborate? Let's Talk!</h1>
-      <div className="contact--para animate__animated animate__zoomInLeft">
-        <p>
-          I enjoy working on React projects, and I can create a beautiful React
-          website for you. I'm eager to work on the frontend component of it in
-          partnership with a business or individual. Additionally, I'm
-          interested in freelancing. Let's discuss more!
-        </p>
-      </div>
-      <div className="contact--links">
-        <p>
-          Shoot me an email on{" "}
-          <a
-            href="mailto:dhrumishah951@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="jsx-8b75a9ccf5c40d44 mr-1 inline-flex items-center space-x-1 text-gray-300 transition duration-200 gradient-underline no-underline text-blue-400 hover:text-blue-300 text-blue-400 hover:!text-blue-300"
-          >
-            <span class="jsx-8b75a9ccf5c40d44">dhrumishah951@gmail.com</span>{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="h-4 w-4"
+    <>
+      <Nav />
+      <div>
+        <h1 className="contact--title">Looking to Collaborate? Let's Talk!</h1>
+        <div className="contact--para animate__animated animate__zoomInLeft">
+          <p>
+            I enjoy working on React projects, and I can create a beautiful
+            React website for you. I'm eager to work on the frontend component
+            of it in partnership with a business or individual. Additionally,
+            I'm interested in freelancing. Let's discuss more!
+          </p>
+        </div>
+        <div className="contact--links">
+          <p>
+            Shoot me an email on{" "}
+            <a
+              href="mailto:dhrumishah951@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="jsx-8b75a9ccf5c40d44 mr-1 inline-flex items-center space-x-1 text-gray-300 transition duration-200 gradient-underline no-underline text-blue-400 hover:text-blue-300 text-blue-400 hover:!text-blue-300"
             >
-              <line x1="7" y1="17" x2="17" y2="7"></line>
-              <polyline points="7 7 17 7 17 17"></polyline>
-            </svg>
-          </a>
-        </p>
-        <div className="contact--socials">
-          <img
-            src={Twitter}
-            onClick={twtClick}
-            className="animate__animated animate__bounceInDown"
-          ></img>
-          <img
-            src={Hashnode}
-            onClick={hashClick}
-            className="animate__animated animate__bounceInUp"
-          ></img>
-          <img
-            src={Github}
-            onClick={gitClick}
-            className="animate__animated animate__bounceInDown"
-          ></img>
-          <img
-            src={LinkedIn}
-            onClick={linkedClick}
-            className="animate__animated animate__bounceInUp"
-          ></img>
-        </div>
-      </div>
-      <div className="footer-container">
-        <div className="footer-social">
-          <div className="animate__animated animate__flash animate__infinite animate__slow">
-            <a href="http://twitter.com/deetwts">
-              <img
-                src="https://img.shields.io/twitter/follow/deetwts?label=Twitter&amp;logo=twitter&amp;style=for-the-badge"
-                alt="Twitter badge"
-              ></img>
+              <span class="jsx-8b75a9ccf5c40d44">dhrumishah951@gmail.com</span>{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="h-4 w-4"
+              >
+                <line x1="7" y1="17" x2="17" y2="7"></line>
+                <polyline points="7 7 17 7 17 17"></polyline>
+              </svg>
             </a>
-          </div>
-          <div className="animate__animated animate__flash animate__infinite animate__slow">
-            <a href="https://github.com/dhrumishah?tab=followers">
-              <img
-                src="https://img.shields.io/github/followers/dhrumishah?label=Followers&amp;logo=GitHub&amp;style=for-the-badge"
-                alt="GitHub badge"
-              ></img>
-            </a>
+          </p>
+          <div className="contact--socials">
+            <img
+              src={Twitter}
+              onClick={twtClick}
+              className="animate__animated animate__bounceInDown"
+            ></img>
+            <img
+              src={Hashnode}
+              onClick={hashClick}
+              className="animate__animated animate__bounceInUp"
+            ></img>
+            <img
+              src={Github}
+              onClick={gitClick}
+              className="animate__animated animate__bounceInDown"
+            ></img>
+            <img
+              src={LinkedIn}
+              onClick={linkedClick}
+              className="animate__animated animate__bounceInUp"
+            ></img>
           </div>
         </div>
-        <p>Copyright © Dhrumi Shah 2022, All rights reserved</p>
+        <div className="footer-container">
+          <div className="footer-social">
+            <div className="animate__animated animate__flash animate__infinite animate__slow">
+              <a href="http://twitter.com/deetwts">
+                <img
+                  src="https://img.shields.io/twitter/follow/deetwts?label=Twitter&amp;logo=twitter&amp;style=for-the-badge"
+                  alt="Twitter badge"
+                ></img>
+              </a>
+            </div>
+            <div className="animate__animated animate__flash animate__infinite animate__slow">
+              <a href="https://github.com/dhrumishah?tab=followers">
+                <img
+                  src="https://img.shields.io/github/followers/dhrumishah?label=Followers&amp;logo=GitHub&amp;style=for-the-badge"
+                  alt="GitHub badge"
+                ></img>
+              </a>
+            </div>
+          </div>
+          <p>Copyright © Dhrumi Shah 2022, All rights reserved</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
